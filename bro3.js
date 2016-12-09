a24403d2f497c6964a7427242099ff0cb43d97d8
Rohaan Advani <RohaanAdvani68@gmail.com>
2016-12-09 13:31:51 +0530

    parts 1 and 2 working- images, form. Part 3 grid left
    Signed-off-by: Rohaan Advani <RohaanAdvani68@gmail.com>

--- bro3.js
+++ bro3.js
@@ -7,4 +7,18 @@ $("document").ready(function(){
 		$('#pic2').addClass('hidden');
 		$('#pic1').removeClass('hidden');
 	});
+
+	$("input[name=check]").change(function(){
+		if(this.checked)
+			$("input[type=submit]").prop('disabled',false);
+		else
+			$("input[type=submit]").prop('disabled',true);
+	});
+
+	$("form").submit(function(){
+		console.log($("input[name=username]").val());
+		console.log($("input[name=password]").val());
+		if($("input[name=username]").val()=="admin" && $("input[name=password]").val()=="password")
+			alert("hi admin!");
+	});
 });
